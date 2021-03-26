2c1d2a5d1b6a63fd00d028be7e05a82000c79411
joseSaez <jose.saez@tsoftglobal.com>
2021-03-26 19:29:21 -0300

    termina form equipos

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -12,11 +12,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        /*Parent root = FXMLLoader.load(getClass().getResource("../vistas/frmLogin.fxml"));*/
-        Parent root = FXMLLoader.load(getClass().getResource("../vistas/frmEquipos.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../vistas/frmLogin.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
-        /*primaryStage.setScene(new Scene(root, 520, 400));*/
-        primaryStage.setScene(new Scene(root, 800, 525));
+        primaryStage.setScene(new Scene(root, 520, 400));
         primaryStage.show();
     }
 
